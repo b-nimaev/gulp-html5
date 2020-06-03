304473ba0c0bb99c3591077f55bc1824be4450e4
alexandr balzhinimaev <alexandrbnimaev@gmail.com>
2020-06-03 11:29:16 +0800

    optimizited site for prod

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,10 @@ const   path = {
 			scss: './src/scss/',
 		}
 
+const build = {
+			name: 'eaSY', 				// Project Name
+}
+
 
 
 function serve() {
@@ -33,6 +37,7 @@ function serve() {
     gulp.watch(path.scss + "**/*.scss", gulp.series([sassReload]));
     gulp.watch(path.src + "**/*.html", gulp.series([htmlReload]));
     gulp.watch(path.js + "**/*.js", gulp.series([js]));
+
 };
 
 function htmlReload() {
@@ -51,7 +56,9 @@ function sassReload() {
 
 function imageMin() {
 	return gulp.src(path.img + "**/*")
-	.pipe(imagemin())
+	.pipe(imagemin([
+		imagemin.mozjpeg({quality: 10, progressive: true}),
+		]))
 	.pipe(gulp.dest(path.img))
 }
 
@@ -60,11 +67,23 @@ function js() {
 	.pipe(browserSync.stream())
 }
 
+function minifyCss(){
+	return gulp.src(path.css + "style.css")
+	.pipe(minify('style.min.css'))
+	.pipe(gulp.dest(path.css))
+}
+
 function concatjs() {
 	return gulp.src([path.js+'jquery.js', path.js+'libs/slick.min.js', path.js+'index.js'])
 	.pipe(concat('common.min.js'))
 	.pipe(gulp.dest(path.js))
 }
 
-exports.build = gulp.series(concatjs, imageMin);
+function about(projectName) {
+
+}
+
+exports.image = imageMin;
+exports.sass = gulp.series(sassReload, minifyCss);
+exports.build = gulp.series(minifyCss, concatjs, imageMin);
 exports.default = gulp;
